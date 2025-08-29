ae8e226091d8fdbef8cfdc583f44fcef516bb8fe
Gattamaneni venkata sashank <121079094+sashanka74@users.noreply.github.com>
2025-08-29 11:46:53 +0530

    Functions(Day-1)

---
+++ functions.js
@@ -0,0 +1,12 @@
+function fizzbuzz (num){
+    if (num % 3 == 0 && num % 5 == 0){
+        return "fizzbuzz"
+    }
+    else if (num % 3 == 0){
+        return "fizz"
+    }
+    else if (num % 5 == 0){
+        return "buzz"
+}
+}
+console.log(fizzbuzz(35));
